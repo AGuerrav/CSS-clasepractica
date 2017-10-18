ee1a450cc35bd58b2212df977089d5dfa8ac5f3f
AGuerrav <aaguerravaldes@gmail.com>
2017-10-18 09:47:13 -0300

    clase,triptico,posicionamientocajas

---
+++ app.js
@@ -0,0 +1,9 @@
+//2 4 6 8 10 pares hasta el 20
+//inicia en 2, i sea menor igual a 20 ,
+//for (initial setup; condition; increment){
+//  Bloque de código a ejecutar
+//}
+
+for (var i=2; i<=20; i= i +2 ) { //i+=2, i=i+1 == i++
+  console.log(i); //pide qiue imprima en la consola los numeros respectivos de i(pares)
+}
